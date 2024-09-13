ecd11d19478eb529ac5461b549cd46a249c2b669
Noah Wagner <nowa5285@colorado.edu>
2024-09-12 23:53:11 -0600

    progress1

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -12,7 +12,7 @@ func _input(event):
 			get_parent().add_child(new_projectile)
 			
 			var projectile_forward = Vector2.from_angle(rotation)
-			new_projectile.fire(projectile_forward, 100)
+			new_projectile.fire(projectile_forward, 450)
 			new_projectile.position = $ProjectileRefPoint.global_position
 			
 func _physics_process(delta):
